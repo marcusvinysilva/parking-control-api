9e973532d46e9acab11583b2bd0ba9badb093a89
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-09-19 21:03:25 -0300

    feat: create ParkingSpot DTO

---
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -0,0 +1,30 @@
+package com.api.parkingcontrol.dtos;
+
+import javax.validation.constraints.*;
+
+public class ParkingSpotDto {
+    @NotBlank
+    private String parkingSpotNumber;
+
+    @NotBlank
+    @Size(max = 7)
+    private String licensePlateCar;
+
+    @NotBlank
+    private String brandCar;
+
+    @NotBlank
+    private String modelCar;
+
+    @NotBlank
+    private String colorCar;
+
+    @NotBlank
+    private String responsibleName;
+
+    @NotBlank
+    private String apartment;
+
+    @NotBlank
+    private String block;
+}
